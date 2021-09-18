3d320f839c21e75eeae10842e0b2ad6997ff9acf
sergiupocol <sergiupocol7@gmail.com>
2021-09-18 17:58:41 -0400

    Insert json object to dummy db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://sergiu:123@cluster0.dfh1s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function insertIntoMongoCollection(obj) {
+    client.connect(err => {
+        const collection = client.db("sergiu_test").collection("drip_picture");
+
+        collection.insertOne(obj, function(err, res) {
+            if (err) {
+            console.log("error inserting object :(");
+            throw err;
+            }
+            console.log(obj, " sucessfully inserted!");
+            client.close();
+        });
+    });
+}
+
 const app = express()
 const apiPort = 3000
 
@@ -13,3 +33,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+
+
+test_obj = {"pic_url": "", "likes": 3, "dislikes": 2, "comments": []}
+insertIntoMongoCollection(test_obj);
