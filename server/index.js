453ee5e8f51a2d94f2425fa01cb18b242f115a0b
Jeanie Ng <ng.jeanie021@gmail.com>
2021-09-18 19:16:20 -0400

    Upload images to database. Reference: https://www.geeksforgeeks.org/upload-and-retrieve-image-on-mongodb-using-mongoose/

--- server/index.js
+++ server/index.js
@@ -1,15 +1,80 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const app = express()
-const apiPort = 3000
+// Starting NodeJS
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
+// File System Module
+const fs = require("fs");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// Utilities for working with file and directory paths
+const path = require("path");
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+// Middleware Multer to upload the photo to the server in a folder called `uploads` so we can process it
+const multer = require("multer");
+
+const app = express();
+const apiPort = 3000;
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
+
+// Create connection to database
+mongoose.connect(
+  "mongodb+srv://sergiu:123@cluster0.dfh1s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log("connected");
+  }
+);
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
+
+var upload = multer({ storage: storage });
+
+// Load imageModel
+var imgModel = require("../models/models.js");
+
+app.get("/", (req, res) => {
+  imgModel.find({}, (err, items) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("An error occurred", err);
+    } else {
+      res.render("imagesPage", { items: items });
+    }
+  });
+});
+
+app.post("/", upload.single("image"), (req, res, next) => {
+  var obj = {
+    name: req.body.name,
+    desc: req.body.desc,
+    img: {
+      data: fs.readFileSync(
+        path.join(__dirname + "/uploads/" + req.file.filename)
+      ),
+      contentType: "image/png",
+    },
+  };
+  imgModel.create(obj, (err, item) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // item.save();
+      res.redirect("/");
+    }
+  });
+});
+
+/// Template Stuff
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Set EJS as templating engine
+app.set("view engine", "ejs");
